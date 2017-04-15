f2f27ae7f4fc7df2740b8cc27921f17d3a64f9bf
albodelu <albodelu@gmail.com>
2017-04-15 13:58:05 +0200

    Add anko and kotlin extensions and finish bonus step

--- app/src/main/java/com/ardock/first_project_kotlin/MainActivity.kt
+++ app/src/main/java/com/ardock/first_project_kotlin/MainActivity.kt
@@ -2,11 +2,15 @@ package com.ardock.first_project_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_bonus.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_bonus)
+
+        button.setOnClickListener { toast("Message: ${input.text}") }
     }
 }
